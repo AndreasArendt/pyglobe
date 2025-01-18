3c292d643b5525d41a127d7a81c6059a7a76ed70
aarendt <andi.arendt@icloud.com>
2025-01-18 17:34:48 +0100

    cleanup

--- src/gui.py
+++ src/gui.py
@@ -1,47 +1,43 @@
 import pyglobe.pyglobe as map
 from tkinter import *
 from PIL import ImageTk, Image
-import tkinter as tk
-from PIL import Image, ImageTk
 import pyglobe.pyglobe as map
 
 class ImageZoomApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Image Zoom")
+        self.root.title("pyglobe")
 
         # Create a Canvas widget
-        self.canvas = tk.Canvas(self.root, bg="white")
-        self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.canvas = Canvas(self.root, bg="white")
+        self.canvas.pack(fill=BOTH, expand=True)
 
         # Initialize image and image reference
         self.image = None
         self.tk_image = None
 
-        # Load an initial image (you can use your own image file)
-        self.load_image()
+        tile = self.getDefaultTile()
+        self.updateMapTile(tile)
 
         # binding mouse wheel for zooming
         # TODO: properly support MacOs and unix systems <Button-4> and <Button-5> https://github.com/AndreasArendt/pyglobe/issues/1
         self.canvas.bind("<MouseWheel>", self.mapZoom)
 
-    def load_image(self):
-        # Load the image using PIL (Python Imaging Library)
+    def getDefaultTile(self):
         # Get tile information
         x, y = map.wgs2tile(51, 0)
-        tile_url = map.getTileUrl(x=x, y=y, z=8)
-        tile_bytes = map.getTileBytes(tile_url)
+        tileUrl = map.getTileUrl(x=x, y=y, z=8)
+        return map.getTileBytes(tileUrl)
 
-        self.image = Image.open(tile_bytes)
+    def updateMapTile(self, tile):
+        self.image = Image.open(tile)
         self.tk_image = ImageTk.PhotoImage(self.image)
-
-        # Display the image on the Canvas
-        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.tk_image)
+        self.canvas.create_image(0, 0, anchor=NW, image=self.tk_image)
 
     def mapZoom(self, event=None):
         print(event.delta)
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    root = Tk()
     app = ImageZoomApp(root)
     root.mainloop()
